7669b4b2274d0ab457155354780743ed2f3dab3e
Vinicius L S Braz <vinicius_lsb@live.com>
2022-06-11 12:03:49 -0300

    styles changes

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -25,19 +25,36 @@ export const Header = styled.h1`
   margin-bottom: 3rem;
 `;
 
-export const LoadingPhotosProcess = styled.div`
+export const WarningProcess = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  
+  margin-top: 20rem;
+
   font-size: 5rem;
 
   .circle-loading {
     margin-top: 2rem;
     
     animation-name: spin;
-    animation-duration: 5000ms;
+    animation-duration: 2000ms;
     animation-iteration-count: infinite;
     animation-timing-function: linear; 
   }
+  @keyframes spin {
+    from {
+        transform:rotate(0deg);
+    }
+    to {
+        transform:rotate(360deg);
+    }
+  } 
+`;
+
+export const PhotoList = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 1rem;
 `;
--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,43 @@
-import { Container, Content, Header, LoadingPhotosProcess } from './App.styles';
+import { Container, Content, Header, WarningProcess,
+  PhotoList } from './App.styles';
 import { CircleNotch } from 'phosphor-react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { Photo } from "./types/Photo";
+import * as Photos from "./services/photos";
 
 function App() {
   const [allPhotosLoading, setAllPhotosLoading] = useState(false);
+  const [photos, setPhotos] = useState<Photo[]>([]);
 
+  useEffect(() => {
+    const getPhotos = async () => {
+      setAllPhotosLoading(true);
+      setPhotos(await Photos.getAll());
+
+      setAllPhotosLoading(false);
+    }
+    getPhotos();
+  }, []);
+  
   return (
     <Container>
       <Content>
         <Header>Photo Gallery</Header>
         {allPhotosLoading &&
-          <LoadingPhotosProcess>
+          <WarningProcess>
             Loading photos, please wait
             <CircleNotch className='circle-loading' />
-          </LoadingPhotosProcess>
+          </WarningProcess>
         }
+        {(!allPhotosLoading && photos.length > 0 ) &&
+          <PhotoList>
+            {photos.map((item, index) =>(
+              <div key={index}>{item.name}</div>
+            ))}
+          </PhotoList>
+        }
+        {!allPhotosLoading && photos.length === 0 &&
+          <WarningProcess>There aren't photos yet!</WarningProcess>}   
       </Content>
     </Container>
   );
